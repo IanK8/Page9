50f53896892d8590355e64cfe80d2f23d48067db
IanKalotkin <114188361+IanK8@users.noreply.github.com>
2024-02-07 14:37:58 -0500

    eeffoc
    and its funny!

---
+++ Page9.java
@@ -0,0 +1,14 @@
+/*******
+ * Page9
+ * Ian Kalotkin
+ * 2/7/2024
+ * Page9.java
+ ******/
+public class Page9
+{
+    public static void main(String[]args)
+    {
+        System.out.println("Hi");
+        System.out.println("Page9");
+    }
+}
